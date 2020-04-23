58aed471c471495991837dc1fcec8fafbf013311
Paul K <pik@umich.edu>
2020-04-23 11:33:26 +0200

    Added URL Builder

--- src/main/java/guru/springframework/springrestclientexamples/service/APIServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/APIServiceImpl.java
@@ -2,23 +2,31 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.springrestclientexamples.domain.User;
 import guru.springframework.springrestclientexamples.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
-public class APIServiceImpl implements  APIService{
+public class APIServiceImpl implements APIService {
 
 	private final RestTemplate restTemplate;
+	private final String apiUrl;
 
-	public APIServiceImpl(RestTemplate restTemplate) {
+	public APIServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
 		this.restTemplate = restTemplate;
+		this.apiUrl = apiUrl;
 	}
 
 	@Override
 	public List<User> getUsers(Integer limit) {
-		UserData userData = restTemplate.getForObject("http://private-anon-1df0e8173a-apifaketory.apiary-mock.com/api/user?limit=" + limit, UserData.class);
+		UriComponentsBuilder uriComponentsBuilder =
+				  UriComponentsBuilder.fromUriString(apiUrl)
+							 .queryParam("limit", limit);
+		UserData userData = restTemplate
+				  .getForObject(uriComponentsBuilder.toUriString(), UserData.class);
 		return userData.getData();
 	}
 }
